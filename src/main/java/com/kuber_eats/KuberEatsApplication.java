f352523b258597f70cf92fa3977113ce3e3ab606
salman <salman700gada@gmail.com>
2025-09-07 11:23:57 +0530

    initial commit and service setups

---
+++ src/main/java/com/kuber_eats/KuberEatsApplication.java
@@ -0,0 +1,13 @@
+package com.kuber_eats;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KuberEatsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KuberEatsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kuber_eats/KuberEatsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kuber_eats;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KuberEatsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
